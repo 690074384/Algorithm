cd2001057a5e9de46d3484dde6de7c4d55ae3620
690074384 <690074384@qq.com>
2017-11-29 11:04:57 +0800

    冒泡、选择两种方法

--- src/com/lph/sort/BubbleSort.java
+++ src/com/lph/sort/BubbleSort.java
@@ -9,13 +9,12 @@ public class BubbleSort {
     public static void main(String[]args){
         int[]a = new int[]{3,1,2,4,5};
         sort(a);
-        for (int i=0;i<a.length;i++)
-        System.out.print(a[i]);
+        for (int anA : a) System.out.print(anA);
     }
 
 
 
-    public static void sort(int[]a){
+    private static void sort(int[] a){
         int temp;
         for (int i=0;i<a.length-1;i++){
             boolean flag = false;
@@ -30,6 +29,22 @@ public class BubbleSort {
             if(!flag){
                 break;
             }
+        }
+    }
+
+    public static void BubbleSort2(int[] a, int n) {
+        int temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                if (a[j - 1] > a[j]) {
+                    temp = a[j - 1];
+                    a[j - 1] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
         }
     }
 }
--- src/com/lph/sort/SelectSort.java
+++ src/com/lph/sort/SelectSort.java
@@ -7,8 +7,7 @@ public class SelectSort {
     public static void main(String[]args){
         int[]a = new int[]{3,1,2,4,5};
         sort(a);
-        for (int i=0;i<a.length;i++)
-            System.out.print(a[i]);
+        for (int anA : a) System.out.print(anA);
     }
 
     private static void sort(int[] a) {
@@ -25,6 +24,25 @@ public class SelectSort {
                 a[flag] = a[i];
                 a[i] = temp;
             }
+        }
+    }
+
+
+    public static void selectSort(int[] a, int n){
+        int temp;
+        for(int i=0;i<n;i++){
+            int index = i;
+            for (int j=i+1;j<n;j++){
+                if (a[j]<a[index]){
+                    index = j;
+                }
+            }
+            temp = a[i];
+            a[i] = a[index];
+            a[index] = temp;
+        }
+        for (int i = 0;i<n;i++){
+            System.out.print(a[i]+" ");
         }
     }
 }
